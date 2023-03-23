ecdc267eb9470cc559a15ffbdd95495f2e0d16b0
Racsooo3 <oscarchoi128@gmail.com>
2023-03-23 23:26:13 +0800

    Display debug

--- Assets/MainMenu/MainMenuTransition.cs
+++ Assets/MainMenu/MainMenuTransition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MainMenuTransition : MonoBehaviour
 {
@@ -10,7 +11,8 @@ public class MainMenuTransition : MonoBehaviour
 
     private void Start()
     {
-        TransitionStart();
+        TransitionReturn();
+        //TransitionStart();
     }
 
     private List<Transform> GetListOfChildren(Transform parent)
@@ -37,11 +39,50 @@ public class MainMenuTransition : MonoBehaviour
 
     public void TransitionStart()
     {
+        List<Transform> rectangleList = GetListOfChildren(rectangleParent);
+
+        foreach (Transform rectangle in rectangleList)
+        {
+            rectangle.GetComponent<RectTransform>().localScale = new Vector3(0, 0, 0);
+        }
+
         rectangleParent.gameObject.SetActive(true);
         if (Transition(-0.25f) > 0)
         {
             Invoke("TransitionStart", transitionTimePerTick);
         } else
+        {
+            rectangleParent.gameObject.SetActive(false);
+        }
+    }
+
+    private float ReverseTransition(float level)
+    {
+        List<Transform> rectangleList = GetListOfChildren(rectangleParent);
+
+        foreach (Transform rectangle in rectangleList)
+        {
+            rectangle.GetComponent<RectTransform>().localScale -= new Vector3(0, level, 0);
+        }
+
+        return rectangleList[0].GetComponent<RectTransform>().localScale.y;
+    }
+
+    public void TransitionReturn()
+    {
+        List<Transform> rectangleList = GetListOfChildren(rectangleParent);
+
+        foreach (Transform rectangle in rectangleList)
+        {
+            rectangle.GetComponent<RectTransform>().localScale = new Vector3(0, 1, 0);
+        }
+
+        rectangleParent.gameObject.SetActive(true);
+        if (ReverseTransition(-0.25f) < 1)
+        {
+            Invoke("TransitionReturn", transitionTimePerTick);
+        }
+        else
         {
             rectangleParent.gameObject.SetActive(false);
         }
